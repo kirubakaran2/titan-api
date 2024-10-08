4038fbf4f1541fed567179481621d871a0a66a33
Rakesh Kumar <70006331+0xrakesh@users.noreply.github.com>
2024-10-08 21:36:21 +0530

    Update sender.js

--- Router/sender.js
+++ Router/sender.js
@@ -1,22 +1,30 @@
 require("dotenv").config();
-let authToken = process.env.AUTH;
-let accountSid = process.env.SID;
-let phone = process.env.PHONE;
+let authToken = process.env.FAST2SMS;
 const Message = require("../Schema/messages")
-
-const client = require('twilio')(accountSid, authToken);
 
 exports.messager = async (body, to, reason) => {
     try {
-        const message = await client.messages.create({
-            body: body,
-            from: `${phone}`,
-            to: `+91${to}`
-        });
+
+        let requestData = {
+            "route":"q",
+            "message":body,
+            "flash":0,
+            "numbers":to
+
+        }
+
+        const response = await fetch("https://www.fast2sms.com/dev/bulkV2",{
+            method:"POST",
+            headers: {
+                "authorization":authToken,
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(requestData)
+        })
+
 
         const record = new Message({
             phone: to,
-            sid: message.sid,
             message: body,
             time: new Date(),
             reason: reason
