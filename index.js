34abf7ffb153da7aa0a2bde9b77e4b518943037b
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2024-10-30 21:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const cors = require("cors");
 const path = require("path")
 const fs = require("fs")
-
 const app = express()
 const {createUser, userList, user,deleteUser, userSearch, nonactive, active, edit, Admindashboard} = require('./Router/customer')
 const {db} = require("./Router/database")
