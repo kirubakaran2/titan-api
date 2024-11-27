b02aefb2bae419deefb6abeffa03a9942f056921
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2024-11-27 09:55:03 +0530

    Update birthdaywishes.js

--- Router/messages/birthdaywishes.js
+++ Router/messages/birthdaywishes.js
@@ -2,20 +2,26 @@ const Customer = require("../../Schema/customer");
 const { messager } = require("../sender");
 const mongoose = require("mongoose");
 const cron = require("node-cron");
+
 const sentBirthdayNumbers = new Set();
+
 const birthdayWishes = async () => {
     const today = new Date();
     const todayDateString = `${today.getUTCMonth() + 1}-${today.getUTCDate()}`;
+
     try {
         const users = await Customer.find();
+
         for (const user of users) {
             const dob = new Date(user.DOB);
             const dobDateString = `${dob.getUTCMonth() + 1}-${dob.getUTCDate()}`;
+
             if (dobDateString === todayDateString) {
                 if (sentBirthdayNumbers.has(user.PHONE)) {
                     console.log(`Birthday wish already sent to: ${user.NAME}`);
                     continue;
                 }
+
                 const msg = `Happy Birthday, ${user.NAME}! 🎉🎂
                 
                 Wishing you a wonderful day filled with joy and happiness. Thank you for being a part of our Titan Fitness family!
@@ -36,11 +42,9 @@ const birthdayWishes = async () => {
         console.error("Error sending birthday wishes:", err);
     }
 };
-
-cron.schedule("0 9 * * *", () => {
+cron.schedule("5 9 * * *", () => {
     console.log("Running daily birthday wish check...");
     sentBirthdayNumbers.clear();
     birthdayWishes();
 });
-
 exports.birthdayWishes = birthdayWishes;
